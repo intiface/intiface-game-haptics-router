1a6a15095591ef30f605875674d4d36ef091b822
Kyle Machulis <kyle@machul.is>
2019-09-28 14:54:30 -0700

    build: Update dependencies and version to v4
    Adds Motorbunny Support

--- IntifaceGameHapticsRouter/AboutControl.xaml.cs
+++ IntifaceGameHapticsRouter/AboutControl.xaml.cs
@@ -14,7 +14,7 @@ namespace IntifaceGameHapticsRouter
     /// </summary>
     public partial class AboutControl : UserControl
     {
-        private string _currentVersion = "v3";
+        private string _currentVersion = "v4";
 
         public AboutControl()
         {
